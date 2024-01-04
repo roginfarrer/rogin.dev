ebfd7bc83acfc8f20e5e82a3138fad8b76a37992
Rogin Farrer <rfarrer@wayfair.com>
2024-01-03 18:43:16 -0800

    try using vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,23 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [icon(), mdx(), sitemap(), react(), solidJs()],
   vite: {
-    plugins: [vanillaExtractPlugin()]
+    plugins: [vanillaExtractPlugin()],
   },
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
+
