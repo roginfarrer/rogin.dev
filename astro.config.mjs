55475b8dbaca26b949c18fc543a83fb4aad09867
Rogin Farrer <rfarrer@wayfair.com>
2024-01-09 20:51:37 -0800

    remove astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@astrojs/react";
-import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [icon(), mdx(), sitemap(), react(), solidJs()],
+  integrations: [mdx(), sitemap(), react(), solidJs()],
   vite: {
     plugins: [vanillaExtractPlugin()],
   },
