bf061078d507253631c5a0d335d6cdc36cf727a1
Rogin Farrer <rfarrer@wayfair.com>
2024-05-02 13:58:06 -0700

    remove more things

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: "Rogin Farrer",
+    description: "Rogin Farrer is a frontend engineer based in San Diego, CA.",
     site: context.site!,
     items: posts.map((post) => ({
       ...post.data,
